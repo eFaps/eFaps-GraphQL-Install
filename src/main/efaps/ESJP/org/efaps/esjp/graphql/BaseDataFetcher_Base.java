e3ddd51f2b17a58034852dd38068fb1474f70021
Jan Moxter <jan.moxter@moxter.net>
2020-06-29 23:03:32 -0500

    Update base fetcher

--- src/main/efaps/ESJP/org/efaps/esjp/graphql/BaseDataFetcher_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/graphql/BaseDataFetcher_Base.java
@@ -21,15 +21,21 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Properties;
 
+import org.apache.commons.lang3.StringUtils;
 import org.efaps.admin.program.esjp.EFapsApplication;
 import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.db.Instance;
 import org.efaps.eql.EQL;
+import org.efaps.esjp.common.properties.PropertiesUtil;
+import org.efaps.esjp.db.InstanceUtils;
 import org.efaps.graphql.definition.FieldDef;
 import org.efaps.graphql.definition.ObjectDef;
 import org.efaps.graphql.providers.DataFetcherProvider;
 
 import graphql.GraphQLContext;
+import graphql.execution.DataFetcherResult;
 import graphql.schema.DataFetcher;
 import graphql.schema.DataFetchingEnvironment;
 import graphql.schema.GraphQLList;
@@ -46,13 +52,19 @@ public abstract class BaseDataFetcher_Base
     public Object get(final DataFetchingEnvironment _environment)
         throws Exception
     {
-        final List<Map<String, Object>> ret = new ArrayList<>();
+        final var resultBldr = DataFetcherResult.newResult();
+        final List<Map<String, Object>> values = new ArrayList<>();
         final var fieldName = _environment.getFieldDefinition().getName();
         final var parentTypeName = _environment.getExecutionStepInfo().getFieldContainer().getName();
         final String contextKey = DataFetcherProvider.contextKey(parentTypeName, fieldName);
-        final var properties = ((GraphQLContext) _environment.getContext()).getOrDefault(contextKey,
+        final var props = ((GraphQLContext) _environment.getContext()).getOrDefault(contextKey,
                         new HashMap<String, String>());
-        final var type = properties.get("Type01");
+        final var properties = new Properties();
+        properties.putAll(props);
+        final Map<Integer, String> types = PropertiesUtil.analyseProperty(properties, "Type", 0);
+        final Map<Integer, String> linkFroms = PropertiesUtil.analyseProperty(properties, "LinkFrom", 0);
+
+        final var localContext = getLocalContext(_environment);
 
         GraphQLType graphQLType = _environment.getExecutionStepInfo().getFieldDefinition().getType();
         if (graphQLType instanceof GraphQLList) {
@@ -62,11 +74,20 @@ public abstract class BaseDataFetcher_Base
         final Optional<ObjectDef> objectDefOpt = ((GraphQLContext) _environment.getContext()).getOrEmpty(graphTypeName);
         if (objectDefOpt.isPresent()) {
             final var objectDef = objectDefOpt.get();
-            final var print = EQL.builder().print().query(type).select();
+            final var query = EQL.builder().print().query(types.values().toArray(new String[types.values().size()]));
+            if (!linkFroms.isEmpty()) {
+                final Instance parentInstance = (Instance) ((Map<?, ?>)_environment.getSource()).get("currentInstance");
+                if (InstanceUtils.isValid(parentInstance)) {
+                    query.where().attr(linkFroms.values().iterator().next()).eq(parentInstance);
+                }
+            }
+            final var print = query.select();
             for (final var selectedField : _environment.getSelectionSet().getFields()) {
                 if (objectDef.getFields().containsKey(selectedField.getName())) {
                     final FieldDef fieldDef = objectDef.getFields().get(selectedField.getName());
-                    print.select(fieldDef.getSelect()).as(selectedField.getName());
+                    if (StringUtils.isNotBlank(fieldDef.getSelect())) {
+                        print.select(fieldDef.getSelect()).as(selectedField.getName());
+                    }
                 }
             }
             final var eval = print.evaluate();
@@ -75,8 +96,22 @@ public abstract class BaseDataFetcher_Base
                 for (final var selectedField : _environment.getSelectionSet().getFields()) {
                     map.put(selectedField.getName(), eval.get(selectedField.getName()));
                 }
-                ret.add(map);
+                map.put("currentInstance", eval.inst());
+                values.add(map);
             }
+        }
+        return resultBldr.data(values)
+                        .localContext(localContext)
+                        .build();
+    }
+
+    protected Map<String, Object> getLocalContext(final DataFetchingEnvironment _environment)
+    {
+        Map<String, Object> ret;
+        if (_environment.getLocalContext() == null) {
+            ret = new HashMap<>();
+        } else {
+            ret = _environment.getLocalContext();
         }
         return ret;
     }
